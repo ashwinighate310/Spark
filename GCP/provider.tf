8d3db203d528292616763da5c8c1e4fe8f82cef4
Ashwini Ghate <45309795+ashwinighate310@users.noreply.github.com>
2020-12-29 23:59:13 +0800

    Delete provider.tf

--- GCP/provider.tf
+++
@@ -1,8 +0,0 @@
-
-variable "path" {default = "/home/udemy/terraform/credentials"}
-
-provider "google" {
-    project = "festive-zoo-239708"
-    region = "europe-west2-a"
-    credentials = "${file("${var.path}/secrets.json")}"
-}
